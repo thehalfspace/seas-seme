40b4877e25a50436b1443073ada14279f826db00
thehalfspace <thehalfspace@protonmail.com>
2026-02-23 16:03:22 -0500

    enforce system cuda

--- set_envs.sh
+++ set_envs.sh
@@ -1,2 +1,12 @@
-#!/bin/bash
-export JULIA_AMGX_PATH=/oscar/scratch/pthakur8/sem/seas-seme/deps/amgx/build/libamgxsh.so
+# load modules
+module load julia cuda
+
+# Set environments
+export PROJECT_ROOT=/oscar/scratch/pthakur8/sem/seas-seme
+export AMGX_DIR=$PROJECT_ROOT/deps/amgx
+export JULIA_AMGX_PATH=$AMGX_DIR/build/libamgxsh.so
+
+# Set LD LIBRARY PATH
+export LD_LIBRARY_PATH=$AMGX_DIR/build:$LD_LIBRARY_PATH
+
+echo "AMGX Environment Configured!"
